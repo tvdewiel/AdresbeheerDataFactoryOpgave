2b5a33edf615f68cd2f8864e88e55e712b33b566
Tom Vande Wiele <tvdewiel@hotmail.com>
2021-12-05 10:27:28 +0100

    Add project files.

---
+++ AdresbeheerEFlayer/AdresbeheerContext.cs
@@ -0,0 +1,29 @@
+﻿using AdresbeheerEFlayer.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdresbeheerEFlayer
+{
+    public class AdresbeheerContext : DbContext
+    {
+        private string connectionString;
+
+        public AdresbeheerContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+      
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ AdresbeheerEFlayer/Repositories/GemeenteRepositoryEF.cs
@@ -0,0 +1,101 @@
+﻿using AdresbeheerDomain.Interfaces;
+using AdresbeheerDomain.Model;
+using AdresbeheerEFlayer.Exceptions;
+using AdresbeheerEFlayer.Mappers;
+using AdresbeheerEFlayer.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdresbeheerEFlayer.Repositories
+{
+    public class GemeenteRepositoryEF : IGemeenteRepository
+    {
+        private AdresbeheerContext ctx;
+
+        public GemeenteRepositoryEF(string connectionString)
+        {
+            this.ctx = new AdresbeheerContext(connectionString);
+        }
+
+        public Gemeente GeefGemeente(int id)
+        {
+            try
+            {
+                return null;
+            }
+            catch(Exception ex)
+            {
+                throw new GemeenteRepositoryException("GeefGemeente", ex);
+            }
+        }
+        public List<Gemeente> GeefGemeenten()
+        {
+            try
+            {
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new GemeenteRepositoryException("HeeftGemeente", ex);
+            }
+        }
+        public bool HeeftGemeente(int id)
+        {
+            try
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new GemeenteRepositoryException("HeeftGemeente", ex);
+            }
+        }
+        public bool HeeftStraten(int id)
+        {
+            try
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new GemeenteRepositoryException("HeeftGemeente", ex);
+            }
+        }
+        public void UpdateGemeente(Gemeente gemeente)
+        {
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                throw new GemeenteRepositoryException("HeeftGemeente", ex);
+            }
+        }
+        public void VerwijderGemeente(int id)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                throw new GemeenteRepositoryException("VerwijderGemeente", ex);
+            }
+        }
+        public void VoegGemeenteToe(Gemeente gemeente)
+        {
+            try { 
+               
+            }
+            catch (Exception ex)
+            {
+                throw new GemeenteRepositoryException("VoegGemeenteToe", ex);
+            }
+        }
+    }
+}
